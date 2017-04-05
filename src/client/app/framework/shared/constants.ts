eb9aeaadf73bdd2f31890af0ebf48b61840d0c3c
ShrikantPatki <shrikant.patki@techprimelab.com>
2017-04-05 21:42:43 +0530

    remove " "  to ''

--- src/client/app/framework/shared/constants.ts
+++ src/client/app/framework/shared/constants.ts
@@ -1,8 +1,9 @@
 export class AppSettings {
   //public static IP = 'http://localhost:8080';
 // public static IP = 'http://ee802b7f.ngrok.io';
- public static IP = 'http://10.192.33.252:8087';
+// public static IP = 'http://10.192.33.252:8087';
   //public static IP = 'http://54.167.209.10:8080';
+  public static IP = '';
 
     public static get API_ENDPOINT():string {
         return this.IP + '/api/';
