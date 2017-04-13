2b07079bfeb2ad13a365a98ca05be1339e5ae2e0
krishna <chetan.unkule@techprimelab.com>
2017-04-13 19:04:08 +0530

    close button functionality and job poster working

--- src/client/app/framework/shared/constants.ts
+++ src/client/app/framework/shared/constants.ts
@@ -1,8 +1,8 @@
 export class AppSettings {
-  public static IP = 'http://localhost:8080';
+  //public static IP = 'http://localhost:8080';
   // public static IP = 'http://ee802b7f.ngrok.io';
 // public static IP = 'http://10.192.33.252:8087';
- // public static IP = 'http://52.89.221.23:8080';
+  public static IP = 'http://52.89.221.23:8080';
   //public static IP = '';
 
 
@@ -150,8 +150,6 @@ export class API {
   public static NOTICEPERIOD='noticeperiod';
   public static PROFICIENCYLIST='proficiency';
   public static DOMAINLIST='domain';
-
-
   public static CAPABILITY_LIST = 'capability';
   public static ROLE_LIST = 'roles';
   public static COMPANY_DETAILS:string = 'companydetails';
