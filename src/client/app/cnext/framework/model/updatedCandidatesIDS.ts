5169a7cccc477de55852e447d43a673a24fd2f0c
Lucky <lucky.vaishnav@techprimelab.com>
2017-05-17 10:28:19 +0530

    updateid modal

---
+++ src/client/app/cnext/framework/model/updatedCandidatesIDS.ts
@@ -0,0 +1,6 @@
+export class UpdatedIds {
+  public updatedCandidateIncartId:string;
+  public updatedCandidateInShortlistId:string;
+  public updatedCandidateRejectedId:string;
+  public removeCandidateIncartId:string;
+}
