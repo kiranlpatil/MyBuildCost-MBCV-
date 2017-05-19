ee0131dfae30404fc2e290957b21f8479dff3193
ShrikantPatki <shrikant.patki@techprimelab.com>
2017-05-19 18:45:51 +0530

    blocked jobs are visible

--- src/client/app/cnext/framework/candidate-dashboard/candidate-job-list/candidate-job-list.service.ts
+++ src/client/app/cnext/framework/candidate-dashboard/candidate-job-list/candidate-job-list.service.ts
@@ -1,27 +1,29 @@
 import {Injectable} from "@angular/core";
-import {Http, Headers, RequestOptions} from "@angular/http";
-import {Observable} from "rxjs";
+import {Http} from "@angular/http";
 import {BaseService} from "../../../../framework/shared/httpservices/base.service";
 import {API, LocalStorage, ValueConstant} from "../../../../framework/shared/constants";
 import {LocalStorageService} from "../../../../framework/shared/localstorage.service";
 
 @Injectable()
-export class   CandidateJobListService extends BaseService{
+export class CandidateJobListService extends BaseService {
 
   constructor(private http:Http) {
     super();
   }
+
   getAppliedJobList() {
-    let url:string = API.CANDIDATE_PROFILE + '/' + LocalStorageService.getLocalValue(LocalStorage.END_USER_ID) + '/list/'+ValueConstant.APPLIED_CANDIDATE;
-    return this.http.get(url)
-      .map(this.extractData)
-      .catch(this.handleError);
-  }
-  getBlockedJobList() {
-      let url:string = API.CANDIDATE_PROFILE + '/' + LocalStorageService.getLocalValue(LocalStorage.END_USER_ID) + '/jobProfile/'+ValueConstant.BLOCKED_CANDIDATE;
+    let url:string = API.CANDIDATE_PROFILE + '/' + LocalStorageService.getLocalValue(LocalStorage.END_USER_ID) + 
+      '/list/' + ValueConstant.APPLIED_CANDIDATE;
     return this.http.get(url)
       .map(this.extractData)
       .catch(this.handleError);
   }
 
+  getBlockedJobList() {
+    let url:string = API.CANDIDATE_PROFILE + '/' + LocalStorageService.getLocalValue(LocalStorage.END_USER_ID) +
+      '/list/' + ValueConstant.BLOCKED_CANDIDATE;
+    return this.http.get(url)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
 }
