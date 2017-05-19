e65387c37f23e581297e6b56562ad5e0fd599670
ShrikantPatki <shrikant.patki@techprimelab.com>
2017-05-19 16:13:08 +0530

    removed ppst job

--- src/client/app/cnext/framework/one-page-summury/recruiter-job-summary/recruiter-job-information/recruiter-job-information.component.ts
+++ src/client/app/cnext/framework/one-page-summury/recruiter-job-summary/recruiter-job-information/recruiter-job-information.component.ts
@@ -1,6 +1,6 @@
 import {Component,  Input} from "@angular/core";
 import {JobSummary} from "../../../model/jobSummary";
-import {ImagePath} from "../../../../../framework/shared/constants";
+import {ImagePath, AppSettings} from "../../../../../framework/shared/constants";
 
 @Component({
   moduleId: module.id,
@@ -11,14 +11,17 @@ import {ImagePath} from "../../../../../framework/shared/constants";
 
 export class RecruiterJobInformation  {
   @Input() recruiter: JobSummary=new JobSummary() ;
-  private image_path:string=ImagePath.COMPANY_LOGO_IMG_ICON;
+  private image_path:string=ImagePath.PROFILE_IMG_ICON;
 
-  ngOnChanges(changes:any) {
+  ngOnChanges(changes:any) {debugger
     if(changes.recruiter.currentValue != undefined){
       this.recruiter=changes.recruiter.currentValue;
     }
     if(this.recruiter != undefined && this.recruiter.company_logo != undefined){
-      this.image_path=this.recruiter.company_logo;
+      this.image_path= AppSettings.IP + this.recruiter.company_logo .substring(4).replace('"', '');;
     }
+
+
+
   }
 }
--- src/client/app/framework/shared/constants.ts
+++ src/client/app/framework/shared/constants.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-  //public static IP = 'http://10.192.33.139:8080';
+  //public static IP = 'http://localhost:8080';
   // public static IP = 'http://ee802b7f.ngrok.io';
   //public static IP = 'http://10.192.33.252:8087';
   //public static IP = 'http://52.89.221.23:8080';
