92ae4328979e25b9c09df77856fd0581f704808c
kirang <DBp123!>
2017-07-19 18:12:19 +0530

    Corrected few messages in Message.ts

--- src/server/app/framework/shared/messages.ts
+++ src/server/app/framework/shared/messages.ts
@@ -7,10 +7,10 @@ class Messages {
   public static STATUS_ERROR: string = 'Error';
 
   //Email subject
-  public static EMAIL_SUBJECT_REGISTRATION: string = ' Registration successful';
+  public static EMAIL_SUBJECT_REGISTRATION: string = ' Registration successful.';
   public static EMAIL_SUBJECT_CHANGE_EMAILID: string = 'Confirmation for change in email address.';
-  public static EMAIL_SUBJECT_FORGOT_PASSWORD: string = 'Reset your password';
-  public static EMAIL_SUBJECT_USER_CONTACTED_YOU: string = 'User Contacted you';
+  public static EMAIL_SUBJECT_FORGOT_PASSWORD: string = 'Reset your password.';
+  public static EMAIL_SUBJECT_USER_CONTACTED_YOU: string = 'User contacted you.';
 
   //Sendmail
   public static MSG_SUCCESS_EMAIL_REGISTRATION: string = 'Email sent on registered email address.';
@@ -20,32 +20,32 @@ class Messages {
   public static MSG_SUCCESS_EMAIL: string = 'Email has been sent successfully';
   public static MSG_SUCCESS_LOGIN: string = 'You are successfully logged in';
   public static MSG_SUCCESS_REGISTRATION: string = 'Registration successful';
-  public static MSG_SUCCESS_CHANGE_PASSWORD: string = 'Your password has been changed successfully';
+  public static MSG_SUCCESS_CHANGE_PASSWORD: string = 'Your password has been changed successfully.';
   public static MSG_SUCCESS_SUBMITTED: string = 'Form submitted successfully';
   public static MSG_SUCCESS_OTP: string = 'OTP has been sent on your verified mobile number.';
   public static MSG_SUCCESS_OTP_CHANGE_MOBILE_NUMBER: string = 'OTP has been sent on your new mobile number';
 
   //Error Message
-  public static MSG_ERROR_MESSAGE_SENDING: string = 'Message sending failed by server';
-  public static MSG_ERROR_CREATING_USER: string = 'User registration failed';
+  public static MSG_ERROR_MESSAGE_SENDING: string = 'Message sending failed by server.';
+  public static MSG_ERROR_CREATING_USER: string = 'User registration failed.';
   public static MSG_ERROR_REGISTRATION: string = 'User already present with this email address ';
   public static MSG_ERROR_REGISTRATION_MOBILE_NUMBER: string = 'User already present with this mobile number.';//Dont Change It
   public static MSG_ERROR_USER_NOT_FOUND: string = 'There is no user registered with this email address.';//Dont Change it
-  public static MSG_ERROR_USER_NOT_FOUND_Mail_SEND: string = 'Email has been sent successfully on your registered email to reset password.';//Dont Change it
+  public static MSG_ERROR_USER_NOT_FOUND_Mail_SEND: string = 'Email has been sent successfully on your registered email address to reset password.';//Dont Change it
   public static MSG_ERROR_EMAIL_ACTIVE_NOW: string = 'User already has an account associated with this email address.';//DontChnge It
   public static MSG_ERROR_TOKEN_SESSION: string = 'Your session has expired.';
   public static MSG_ERROR_WRONG_TOKEN: string = 'Invalid access token';
   public static MSG_ERROR_INACTIVE_USER: string = 'Inactive user. Kindly click on Register Now!!!.';
   public static MSG_ERROR_WRONG_CURRENT_PASSWORD: string = 'Incorrect current password entered.';
   public static MSG_ERROR_WRONG_CURRENT_EMAIL: string = 'Incorrect current email address entered.';
-  public static MSG_ERROR_PROVIDE_TOKEN: string = 'Provide access token';
-  public static MSG_ERROR_IS_BEARER: string = 'Invalid token strategy';
-  public static MSG_ERROR_PROVIDE_ID: string = 'Provide user ID';
+  public static MSG_ERROR_PROVIDE_TOKEN: string = 'Provide access token.';
+  public static MSG_ERROR_IS_BEARER: string = 'Invalid token strategy.';
+  public static MSG_ERROR_PROVIDE_ID: string = 'Provide user ID..';
   public static MSG_ERROR_ACCOUNT_STATUS: string = 'Your account is still inactive. Verify your account by clicking the verification link sent on your email.';//dont change It
   public static MSG_ERROR_VERIFY_ACCOUNT: string = 'Please click on the link sent to your email in order to activate your account.';//Dont CHange It
   public static MSG_ERROR_VERIFY_CANDIDATE_ACCOUNT: string = 'Please contact the administrator to activate your account.';//Dont CHange It
-  public static MSG_ERROR_INVALID_ID: string = 'Invalid Userid';
-  public static MSG_ERROR_USER_NOT_PRESENT: string = 'Please enter valid login email or password.';
+  public static MSG_ERROR_INVALID_ID: string = 'Invalid User Id';
+  public static MSG_ERROR_USER_NOT_PRESENT: string = 'Please enter valid login email address or password.';
   public static MSG_ERROR_USER_WITH_EMAIL_PRESENT: string = 'You are already registered. Kindly click on sign in.';
   public static MSG_ERROR_DIRECTORY_NOT_FOUND: string = 'Image directory not found.';
   public static MSG_ERROR_INVALID_TOKEN: string = 'Invalid access token';
@@ -54,11 +54,11 @@ class Messages {
   public static MSG_ERROR_PROVIDE_EMAIL: string = 'Please provide email address';
   public static MSG_ERROR_INVALID_CREDENTIALS: string = 'The email address or phone number that you have entered does not match any account.';
   public static MSG_ERROR_WRONG_PASSWORD: string = 'Please enter valid login email or password.';
-  public static MSG_ERROR_NOT_USER: string = 'An access token is expired or is invalid';
-  public static MSG_ERROR_FIELD_VERIFICATION: string = 'Fields Verification Failed';
-  public static MSG_ERROR_FACEBOOK_AUTH: string = 'Facebook Authentication Failed';
-  public static MSG_ERROR_GOOGLE_AUTH: string = 'Google plus Authentication Failed';
-  public static MSG_ERROR_EMPTY_FIELD: string = 'Fields cannot be empty';
+  public static MSG_ERROR_NOT_USER: string = 'Provided access token is expired or is invalid';
+  public static MSG_ERROR_FIELD_VERIFICATION: string = 'Fields verification failed';
+  public static MSG_ERROR_FACEBOOK_AUTH: string = 'Facebook authentication failed';
+  public static MSG_ERROR_GOOGLE_AUTH: string = 'Google plus authentication failed';
+  public static MSG_ERROR_EMPTY_FIELD: string = 'Fields cannot be empty.';
   public static MSG_ERROR_WHILE_CONTACTING: string = 'Something went wrong. Try again.';
   public static MSG_ERROR_CONNECTION_TIMEOUT: string = 'Connection timeout. Try again.';
 
@@ -66,21 +66,21 @@ class Messages {
 
   // Error Reason
   // public static MSG_ERROR_RSN_MESSAGE_NOT_SENT:string = 'message could not send';
-  public static MSG_ERROR_RSN_USER_NOT_FOUND: string = 'User not found';
-  public static MSG_ERROR_USER_NOT_ACTIVATED: string = 'User account is not activated';
-  public static MSG_ERROR_RSN_DIRECTORY_NOT_FOUND: string = 'Directory not found';
+  public static MSG_ERROR_RSN_USER_NOT_FOUND: string = 'User not found.';
+  public static MSG_ERROR_USER_NOT_ACTIVATED: string = 'User account is not activated.';
+  public static MSG_ERROR_RSN_DIRECTORY_NOT_FOUND: string = 'Directory not found.';
   //MSG_ERROR_RSN_REGISTRATION
-  public static MSG_ERROR_RSN_REGISTRATION: string = 'User already present with this email address';
-  public static MSG_ERROR_RSN_INVALID_CREDENTIALS: string = 'Invalid Credentials';
-  public static MSG_ERROR_RSN_INVALID_REGISTRATION_STATUS: string = 'Invalid Registration Status';
-  public static MSG_ERROR_RSN_NOT_ALLOW: string = 'Insufficient user permission to access public profile';
+  public static MSG_ERROR_RSN_REGISTRATION: string = 'User already present with this email address.';
+  public static MSG_ERROR_RSN_INVALID_CREDENTIALS: string = 'Invalid credentials.';
+  public static MSG_ERROR_RSN_INVALID_REGISTRATION_STATUS: string = 'Invalid registration status.';
+  public static MSG_ERROR_RSN_NOT_ALLOW: string = 'Insufficient user permission to access public profile.';
   public static MSG_ERROR_RSN_EXISTING_USER: string = 'User already exists.';
   public static MSG_ERROR_RSN_WHILE_CONTACTING: string = 'There may be a network problem.';
 
   //Verify errors
   public static MSG_ERROR_CHECK_EMAIL_ACCOUNT: string = "Error: User already has an account associated with this email address."; //should be same to MSG_ERROR_EMAIL_ACTIVE_NOW
 
-  public static MSG_ERROR_CHECK_INACTIVE_ACCOUNT: string = "Error: Your account is still inactive. Verify your account by clicking the verification link sent on your email."; //should be same to MSG_ERROR_ACCOUNT_STATUS
+  public static MSG_ERROR_CHECK_INACTIVE_ACCOUNT: string = "Error: Your account is still inactive. Verify your account by clicking the verification link sent to your email address."; //should be same to MSG_ERROR_ACCOUNT_STATUS
   public static MSG_ERROR_CHECK_INVALID_ACCOUNT: string = "Error: There is no user registered with this email address."; // should be same to MSG_ERROR_USER_NOT_FOUND
   public static MSG_ERROR_CHECK_MOBILE_PRESENT: string = "Error: User already present with this mobile number."; //should be same to MSG_ERROR_REGISTRATION_MOBILE_NUMBER
   public static MSG_ERROR_CHECK_EMAIL_PRESENT: string = "Error: Please click on the link sent to your email in order to activate your account. "
