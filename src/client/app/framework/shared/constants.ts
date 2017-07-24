877d246254eafb49ff25d131c916a17cbec3d179
prajakta <prajakta.pawar@techprimelab.com>
2017-07-24 15:23:33 +0530

    CN-921

--- src/client/app/framework/shared/constants.ts
+++ src/client/app/framework/shared/constants.ts
@@ -133,7 +133,7 @@ export class Messages {
   public static MSG_ERROR_VALIDATION_PIN_NUMBER = 'Pin code should not be greater than 20 characters.';
   public static SUGGESTION_MSG_FOR_RELEVENT_INDUSTRY = 'Based on the profile you have selected, we suggest to search candidate from following industries for matching profiles. Remove if you dont want to search candidates from any specific industry.';
   public static SUGGESTION_MSG_ABOUT_DOMAIN =  "In addition to "+ "this.choosedIndeustry" + " industry, do you want the candidate to have mandatory experience in any specific Domain? If yes, select such MUST HAVE DOMAINS from below.";
-  public static MSG_ERROR_VALIDATION_MAX_PROFICIENCIES =  "key skills. Click the cross sign to deselect existing one and add a new skill";
+  public static MSG_ERROR_VALIDATION_MAX_PROFICIENCIES =  " key skills. Click the cross sign to deselect existing one and add a new skill.";
 
 
 }
