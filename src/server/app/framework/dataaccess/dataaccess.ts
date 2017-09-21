afcca30d7086fd0c2509a41876310c0459397981
shrikant <shrikant.patki@techprimelab.com>
2017-09-21 10:59:09 +0530

    changes for staging in dataaccees

--- src/server/app/framework/dataaccess/dataaccess.ts
+++ src/server/app/framework/dataaccess/dataaccess.ts
@@ -1,4 +1,4 @@
-import * as Mongoose from "mongoose";
+import * as Mongoose from 'mongoose';
 let config = require('config');
 
 class DataAccess {
@@ -17,7 +17,7 @@ class DataAccess {
     let host = config.get('TplSeed.database.host');
     let name = config.get('TplSeed.database.name');
     Mongoose.set('debug',true);
-      this.mongooseInstance = Mongoose.connect('mongodb://' + host + '/' + name + '');
+    this.mongooseInstance = Mongoose.connect('mongodb://admin:jobmosisadmin123@' + host + '/' + name+'');
     return this.mongooseInstance;
   }
 }
