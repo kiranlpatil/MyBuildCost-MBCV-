66d9a9cf35a668cf43cffdfb8d83d5dc414eee60
krishna <krish.ghatul007@gmail.com>
2017-09-29 20:56:42 +0530

    dist delete

--- dist/app.server.prod.http2.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by chetan on 24/11/16.
- */
-
-require('./dist/server/prod').init(1300,'prod','http2');
--- dist/app.server.prod2.js
+++
@@ -1,4 +0,0 @@
-/**
- * @ng2 Server Runner `Production`.
- */
-require('./server/prod').init(3000, 'prod', 'http', 'dist');
