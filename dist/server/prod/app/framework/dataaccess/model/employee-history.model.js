95b73c13f8c1ac71addcc31b012451fe7b1a949e
Abhijeet Giram <abhijeet.giram@techprimelab.com>
2017-10-06 19:04:10 +0530

    [CN-1495] Removed dist.

--- dist/server/prod/app/framework/dataaccess/model/employee-history.model.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAvZnJhbWV3b3JrL2RhdGFhY2Nlc3MvbW9kZWwvZW1wbG95ZWUtaGlzdG9yeS5tb2RlbC5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
