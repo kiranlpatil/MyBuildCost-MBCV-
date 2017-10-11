7e8785ef84b7b1408799caf015de00a80fc69090
Rahul Tiwari <shubhamtakode@gmail.com>
2017-10-11 20:12:48 +0530

    [CN-1599]: Close job

--- src/server/app/framework/controllers/job-profile.controller.ts
+++ src/server/app/framework/controllers/job-profile.controller.ts
@@ -243,6 +243,7 @@ export function cloneJob(req: express.Request, res: express.Response, next: any)
         newJob.sharedLink='';
         newJob.postingDate = new Date();
         newJob.candidate_list=[];
+        newJob.isJobPostClosed = false;
 
         newJob.expiringDate = new Date((new Date().getTime() + ConstVariables.JOB__EXPIRIY_PERIOD));
         var recruiterService = new RecruiterService();
