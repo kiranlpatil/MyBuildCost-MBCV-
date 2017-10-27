da2258d3fdeefa22a0a7db57c847b0d1fb9d3164
Abhijeet Giram <abhijeet.giram@techprimelab.com>
2017-10-27 23:53:08 +0530

    [CN-1495] Changed package.json

--- src/server/app/framework/services/cron-job.service.ts
+++ src/server/app/framework/services/cron-job.service.ts
@@ -11,7 +11,7 @@ export class CronJobService {
 
   OnCronJobStart() {
     var mailChimpMailerService=new MailChimpMailerService();
-    var sendingMailOnProfileUpdate = new CronJob('00 10 00 * * 0-6', function () {
+    var sendingMailOnProfileUpdate = new CronJob('00 30 00 * * 0-6', function () {
       mailChimpMailerService. onCronJobCandidateProfileTriggered(2,config.get('TplSeed.mail.CANDIDATE_PROFILE_SUBMIT_25%_LISTID'));
       mailChimpMailerService. onCronJobCandidateProfileTriggered(3,config.get('TplSeed.mail.CANDIDATE_PROFILE_SUBMIT_50%_LISTID'));
       }, null, false,'Asia/Kolkata');
