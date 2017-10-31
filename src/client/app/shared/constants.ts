0ebde74ce959acf171de103514e0205a6c6f79e4
Abhijeet Giram <abhijeet.giram@techprimelab.com>
2017-10-31 10:22:19 +0530

    [CN-1495] Changed constants.ts

--- src/client/app/shared/constants.ts
+++ src/client/app/shared/constants.ts
@@ -2,8 +2,8 @@ export class AppSettings {
   // public static IP = 'http://localhost:8080';
   // public static IP = 'http://ee802b7f.ngrok.io';
   //public static IP = 'http://10.192.33.77:8080';
-  //public static IP = 'https://34.215.90.153:3000';
-  public static IP = 'http://52.10.145.87:3000';
+  public static IP = 'http://34.215.90.153:3000';
+  //public static IP = 'http://52.10.145.87:3000';
   // public static IP = 'app.jobmosis.com';
   // public static IP = '';
 
