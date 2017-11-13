c72bfb1686d246de10114684e9eb44c1eac6371d
Abhijeet Giram <abhijeet.giram@techprimelab.com>
2017-11-13 10:01:50 +0530

    [CN-1495] Changed constants.ts

--- src/server/app/framework/dataaccess/dataaccess.ts
+++ src/server/app/framework/dataaccess/dataaccess.ts
@@ -17,8 +17,8 @@ class DataAccess {
     let host = config.get('TplSeed.database.host');
     let name = config.get('TplSeed.database.name');
     Mongoose.set('debug',true);
-    //this.mongooseInstance = Mongoose.connect('mongodb://admin:jobmosisadmin123@' + host + '/' + name+'');
-    this.mongooseInstance = Mongoose.connect('mongodb://' + host + '/' + name+'');
+    this.mongooseInstance = Mongoose.connect('mongodb://admin:jobmosisadmin123@' + host + '/' + name+'');
+    //this.mongooseInstance = Mongoose.connect('mongodb://' + host + '/' + name+'');
     return this.mongooseInstance;
   }
 }
