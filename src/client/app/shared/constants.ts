8f49f983fd1abfa17d866757eac9a6882c7e90d5
Swapnil <swapnil.nakhate1010@gmail.com>
2018-01-24 15:45:48 +0530

    [BUILDINFO] Change In IP port => 80

--- src/client/app/shared/constants.ts
+++ src/client/app/shared/constants.ts
@@ -1,8 +1,8 @@
 export class AppSettings {
   // public static IP = 'http://localhost:8080';
-  public static IP = 'http://52.66.120.228:8080'; // build info staging
+  public static IP = 'http://52.66.120.228'; // build info staging
   // public static HOST_NAME = 'localhost:8080';
-  public static HOST_NAME = '52.66.120.228:8080';
+  public static HOST_NAME = '52.66.120.228';
 
   public static get API_ENDPOINT(): string {
     return this.IP + '/api/';
