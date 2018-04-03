9d86696d005b3dd3919cac5360329533922b3d1f
Shrishailya Sutar <shrishailya.sutar@techprimelab.com>
2018-04-03 15:18:03 +0530

    [BUILDINFO-505] Create Method structures - UI Create basic structure and filter flow

--- src/client/app/build-info/framework/project/material-takeoff/material-takeoff.component.ts
+++ src/client/app/build-info/framework/project/material-takeoff/material-takeoff.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MaterialTakeoffService } from './material-takeoff.service';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +8,104 @@ import { Component } from '@angular/core';
   templateUrl: 'material-takeoff.component.html'
 })
 
-export class MaterialTakeoffComponent {
+export class MaterialTakeoffComponent implements OnInit {
+
+  projectId : string = null;
+  groupBy : string = 'Cost Head Wise';
+  secondaryFilter : string = 'RCC';
+  secondaryFilterHeading : string = null;
+  secondaryFilterList : any[];
+  building : string = 'Build1';
+
+  flatReport : any[];
+  materialReport : any[];
+
+  public costHeadList: any[] = [
+    'RCC', 'Plaster'
+  ];
+
+  public materialList: any[] = [
+    'Cement', 'Sand'
+  ];
+
+  public buildingList: any[] = [
+    'Building All','Build1', 'Build2'
+  ];
+
+  public groupByList: any[] = [
+    'Cost Head Wise', 'Material Wise'
+  ];
+
+  constructor( private activatedRoute:ActivatedRoute,  private _router : Router, private materialTakeoffService : MaterialTakeoffService) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.projectId = params['projectId'];
+    });
+    this.getList(this.projectId);
+  }
+
+  getList(projectId : string) {
+/*    this.materialTakeoffService.getList(projectId).subscribe(
+      flatReport => this.onGetListSuccess(flatReport),
+      error => this.onGetListFailure(error)
+    );*/
+    /*this.flatReport = [
+      {
+        'building' : '',
+        'costHead' : '',
+        'workItem' : '',
+        'material' : '',
+        'quantity' : '',
+        'label' : '',
+        'unit' : ''
+      }
+    ];
+
+    this.materialReport = this.materialTakeoffService.buildMaterialReport(this.building, this.secondaryFilter,
+      this.groupBy, this.flatReport);*/
+
+    this.secondaryFilterHeading = 'Cost Head';
+    this.secondaryFilterList = this.costHeadList;
+  }
+
+/*  onGetListSuccess(flatReport : any) {
+
+  }
+
+  onGetListFailure(error : any) {
+    console.log(error);
+  }*/
+
+  onChangeGroupBy(groupBy : any) {
+    this.groupBy = groupBy;
+    console.log('Group By :'+this.groupBy);
+    if(this.groupBy === 'Cost Head Wise') {
+      this.secondaryFilterList = this.costHeadList;
+      this.secondaryFilterHeading = 'Cost Head';
+    } else {
+      this.secondaryFilterList = this.materialList;
+      this.secondaryFilterHeading = 'Material';
+    }
+
+    /*this.materialReport = this.materialTakeoffService.buildMaterialReport(this.building, this.secondaryFilter,
+      this.groupBy, this.flatReport);*/
+  }
+
+  onChangeSecondFilter(secondFilter : any) {
+    this.secondaryFilter = secondFilter;
+    console.log('Second Filter :'+this.secondaryFilter);
+
+    /*this.materialReport = this.materialTakeoffService.buildMaterialReport(this.building, this.secondaryFilter,
+      this.groupBy, this.flatReport);*/
+  }
+
+  onChangeBuilding(building : any) {
+    this.building = building;
+    console.log('Building :'+this.building);
+
+    /*this.materialReport = this.materialTakeoffService.buildMaterialReport(this.building, this.secondaryFilter,
+      this.groupBy, this.flatReport);*/
+  }
 }
--- src/client/app/build-info/framework/project/material-takeoff/material-takeoff.service.ts
+++ src/client/app/build-info/framework/project/material-takeoff/material-takeoff.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, } from '@angular/http';
 import { BaseService, MessageService } from '../../../../shared/index';
+import { HttpDelegateService } from '../../../../shared/services/http-delegate.service';
+import { API } from '../../../../shared';
 
 
 @Injectable()
 export class MaterialTakeoffService extends BaseService {
 
-  constructor(protected http: Http, protected messageService: MessageService) {
+  constructor(protected http: Http, protected messageService: MessageService, protected httpDelegateService : HttpDelegateService) {
     super();
   }
 
+  getList(projectId: string) {
+/*    var url = API.PROJECT + '/' +projectId+ '/' + 'materialtakeoff';
+    return this.httpDelegateService.getAPI(url);*/
+  }
+
+  buildMaterialReport(building : string, secondaryFilter : string, groupBy : string, flatReport : any) {
+    /*let materialReport : any[];
+    return materialReport;*/
+  }
 }
