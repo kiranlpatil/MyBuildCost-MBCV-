bf7527104cc3e4e1d094044da2749b6b5f469fd4
swapnil133 <swapnil.kadam@techprimelab.com>
2018-04-13 18:49:50 +0530

    {BUILDINFO-546} Material take off UI fixed

--- src/client/app/build-info/framework/project/material-takeoff/material-take-off-report/row/row.component.ts
+++ src/client/app/build-info/framework/project/material-takeoff/material-take-off-report/row/row.component.ts
@@ -4,12 +4,14 @@ import { MaterialTakeOffElements } from '../../../../../../shared/constants';
 @Component({
   moduleId: module.id,
   selector: 'bi-table-row',
-  templateUrl: 'row.component.html'
+  templateUrl: 'row.component.html',
+  styleUrls: ['row.component.css'],
 })
 
 export class TableRowComponent {
 
   @Input() rows: any;
+  @Input() customClass: string;
 
   getMaterialTakeOffElements() {
     return MaterialTakeOffElements;
