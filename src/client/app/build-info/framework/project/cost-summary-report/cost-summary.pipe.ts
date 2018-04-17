9f34ae6d01fe7dae6848230ffa3563a24cfea13a
Shrishailya Sutar <shrishailya.sutar@techprimelab.com>
2018-04-17 20:02:14 +0530

    [BUILDINFO-597] Sequence of Buildings and floors are not coming in proper order Swapnil's review comment resolved

--- src/client/app/build-info/framework/project/cost-summary-report/cost-summary.pipe.ts
+++ src/client/app/build-info/framework/project/cost-summary-report/cost-summary.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { MaterialTakeOffElements } from '../../../../shared/constants';
 @Pipe({name: 'keys', pure: false})
 
 export class CostSummaryPipe implements PipeTransform {
   transform(value: any, sort : string,args: any[] = null): any {
     if(value !== undefined) {
-      if(sort === 'sort') {
+      if(sort === MaterialTakeOffElements.SORT) {
         return Object.keys(value).sort(function (a, b) {
           return a.toLowerCase().localeCompare(b.toLowerCase());
         });
