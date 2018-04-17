38bef219dd0074b8436253199ca4bc51faaa78b8
Rahul Tiwari <rahul.tiwari@techprimelab.com>
2018-04-17 16:54:52 +0530

    [BUILDINFO-566]: Building selection,Carpet/Saleable/Slab etc wise report to be selected first and then same should be exported in report format

--- src/client/app/build-info/framework/project/report-templates/cost-summary-report/cost-summary-report.component.ts
+++ src/client/app/build-info/framework/project/report-templates/cost-summary-report/cost-summary-report.component.ts
@@ -13,25 +13,54 @@ import {SessionStorage} from "../../../../../shared/constants";
 export class CostSummaryReportComponent {
   @ViewChild('content') content: ElementRef;
   @Input() buildingReport: any;
+  @Input() costingByUnit: any;
+  @Input() costingByArea: any;
   currentProjectName: string;
   constructor() {
     this.currentProjectName = SessionStorageService.getSessionValue(SessionStorage.CURRENT_PROJECT_NAME);
   }
 
   downloadToPdf() {
-    let doc = new jsPDF();
+    let l = {
+      orientation: 'l',
+      unit: 'mm',
+      format: 'a3',
+      compress: true,
+      fontSize: 8,
+      lineHeight: 1,
+      autoSize: false,
+      printHeaders: true
+    };
+
+    let doc = new jsPDF(l, '', '', '');
     let specialElementHandlers = {
-      '#editor': function (element : any, renderer : any) {
+      '#editor': function (element, renderer) {
         return true;
       }
     };
 
+    doc.setProperties({
+      title: 'Test PDF Document',
+      subject: 'This is the subject',
+      author: 'author',
+      keywords: 'generated, javascript, web 2.0, ajax',
+      creator: 'author'
+    });
+
     let content = this.content.nativeElement;
-    doc.fromHTML(content.innerHTML, 10, 10, {
-      'width': 20,
+    doc.fromHTML(content.innerHTML, 12, 15, {
+      'width': 190,
       'elementHandlers': specialElementHandlers
     });
 
+    doc.cellInitialize();
+    doc.margin = 1.5;
+    doc.margins = 1.5;
+    doc.setFont("courier");
+    doc.setFontType("bolditalic");
+    doc.setFontSize(9);
+    doc.cell(2, 12, 35, 9, 0, 0 );
     doc.save('cost-summary-report.pdf');
   }
 }
+
