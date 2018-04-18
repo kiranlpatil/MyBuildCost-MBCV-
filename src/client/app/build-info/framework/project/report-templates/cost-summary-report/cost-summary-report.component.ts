6a6d574c3440b3614a07efe1350486e8091a0fcd
Rahul Tiwari <rahul.tiwari@techprimelab.com>
2018-04-18 12:28:43 +0530

    [BUILDINFO-565]: Print pdf of budgeted and estimated report

--- src/client/app/build-info/framework/project/report-templates/cost-summary-report/cost-summary-report.component.ts
+++ src/client/app/build-info/framework/project/report-templates/cost-summary-report/cost-summary-report.component.ts
@@ -9,19 +9,43 @@ import { SessionStorage } from '../../../../../shared/constants';
 })
 
 export class CostSummaryReportComponent {
-  @ViewChild('content', {read: ElementRef}) content: ElementRef;
+  @ViewChild('budgetedAndEstimated', {read: ElementRef}) budgetedAndEstimated: ElementRef;
+  @ViewChild('budgeted', {read: ElementRef}) budgeted: ElementRef;
+  @ViewChild('estimated', {read: ElementRef}) estimated: ElementRef;
   @Input() buildingReport: any;
   @Input() costingByUnit: any;
   @Input() costingByArea: any;
   currentProjectName: string;
+  isBudgetedAndEstimated: boolean = false;
+  isBudgeted: boolean = false;
+  isEstimated: boolean = false;
 
   constructor() {
     this.currentProjectName = SessionStorageService.getSessionValue(SessionStorage.CURRENT_PROJECT_NAME);
   }
 
-  downloadToPdf() {
+  downloadToPdf(reportType: string) {
+    let content: any;
+    switch (reportType) {
+      case 'Budgeted and Estimated cost report':
+        content = this.budgetedAndEstimated.nativeElement.innerHTML;
+        this.isBudgetedAndEstimated = true;
+        break;
+      case 'Budgeted cost report':
+        content = this.budgeted.nativeElement.innerHTML;
+        this.isBudgeted = true;
+        break;
+      case 'Estimated cost report':
+        content = this.estimated.nativeElement.innerHTML;
+        this.isEstimated = true;
+        break;
+      default:
+        this.isBudgetedAndEstimated = false;
+        this.isBudgeted = false;
+        this.isEstimated = false;
+    }
     let contentDiv = document.createElement('div');
-    let content = this.content.nativeElement.innerHTML;
+    //let content = this.BudgetedAndEstimated.nativeElement.innerHTML;
     contentDiv.innerHTML = content;
     contentDiv.setAttribute('id','print-div');
     document.getElementById('tpl-app').style.display = 'none';
