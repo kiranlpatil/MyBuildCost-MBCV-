407e29e437ff1bb9f5daec938171f47af5f4d59e
Nitesh <nitesh.gotmare@techprimelab.com>
2018-04-20 11:27:52 +0530

    [BUILDINFO-432] stagin up- dist deleted for dev build

--- dist/client/dev/app/framework/registration/activate-user/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./activate-user.component"));
-__export(require("./activate-user.routes"));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9mcmFtZXdvcmsvcmVnaXN0cmF0aW9uL2FjdGl2YXRlLXVzZXIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSwrQ0FBMEM7QUFDMUMsNENBQXVDIiwiZmlsZSI6ImFwcC9mcmFtZXdvcmsvcmVnaXN0cmF0aW9uL2FjdGl2YXRlLXVzZXIvaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL2FjdGl2YXRlLXVzZXIuY29tcG9uZW50JztcclxuZXhwb3J0ICogZnJvbSAnLi9hY3RpdmF0ZS11c2VyLnJvdXRlcyc7XHJcbiJdfQ==
--- dist/client/dev/app/framework/shared/footer/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./footer.component"));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9mcmFtZXdvcmsvc2hhcmVkL2Zvb3Rlci9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUdBLHdDQUFtQyIsImZpbGUiOiJhcHAvZnJhbWV3b3JrL3NoYXJlZC9mb290ZXIvaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcclxuICogQ3JlYXRlZCBieSBUUEwgb24gNy8xLzIwMTYuXHJcbiAqL1xyXG5leHBvcnQgKiBmcm9tICcuL2Zvb3Rlci5jb21wb25lbnQnO1xyXG5cclxuIl19
