a41c8aac1ba97ca309463b4cd0192f50e3b70f7f
Swapnil <swapnil.nakhate1010@gmail.com>
2018-04-24 10:55:30 +0530

    [BUILDINFO-639] Non Estimated workitems not to be shown in the report

--- src/client/app/build-info/framework/project/report-templates/material-take-off-report/pdf-header/pdf-header.component.ts
+++ src/client/app/build-info/framework/project/report-templates/material-take-off-report/pdf-header/pdf-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MaterialTakeOffElements, PDFReportHeaders, ProjectElements, SessionStorage} from '../../../../../../shared/constants';
+import { MaterialTakeOffElements, PDFReportHeaders, ProjectElements, SessionStorage } from '../../../../../../shared/constants';
 import { SessionStorageService } from '../../../../../../shared/services/session.service';
 
 @Component({
