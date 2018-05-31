88063a3d5031149c5bbc6a08332d9ff9eacac160
Nitesh <nitesh.gotmare@techprimelab.com>
2018-05-31 17:54:30 +0530

    [BUILDINFO-432] dist deleted

--- dist/app.server.dev.js
+++
@@ -1,4 +0,0 @@
-/**
- * @ng2 Server Runner `Development`.
- */
-require('./dist/server/dev').init(8080, 'dev','http');
--- dist/server/prod/app/applicationProject/dataaccess/mongoose/Subscription.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAvYXBwbGljYXRpb25Qcm9qZWN0L2RhdGFhY2Nlc3MvbW9uZ29vc2UvU3Vic2NyaXB0aW9uLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
