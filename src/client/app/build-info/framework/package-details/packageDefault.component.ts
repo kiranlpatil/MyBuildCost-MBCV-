4975f475e4d795071ebcae22d3d8e3dfca64fb26
Swapnil <swapnil.nakhate1010@gmail.com>
2018-06-01 15:56:22 +0530

    [BUILDINFO-773] Package files projects

--- src/client/app/build-info/framework/package-details/packageDefault.component.ts
+++ src/client/app/build-info/framework/package-details/packageDefault.component.ts
@@ -2,7 +2,7 @@ import { Component,OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'bi-default-package',
-  template: `<router-outlet></router-outlet>`
+  template: `<tpl-dashboard-header></tpl-dashboard-header><router-outlet></router-outlet>`
 })
 export class PackageDefaultComponent {
 
