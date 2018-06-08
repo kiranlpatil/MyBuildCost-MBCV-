987ff269e6ac0dd49ef983a848315cac382095a6
Swapnil <swapnil.nakhate1010@gmail.com>
2018-06-08 15:52:46 +0530

    [BUILDINFO-432] staging up -dist deleted

--- dist/server/prod/app/framework/dataaccess/model/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var UsersClassModel = (function () {
-    function UsersClassModel() {
-    }
-    return UsersClassModel;
-}());
-module.exports = UsersClassModel;
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9mcmFtZXdvcmsvZGF0YWFjY2Vzcy9tb2RlbC91c2Vycy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7SUFBQTtJQUNBLENBQUM7SUFBRCxzQkFBQztBQUFELENBREEsQUFDQyxJQUFBO0FBQ0QsaUJBQVMsZUFBZSxDQUFDIiwiZmlsZSI6ImFwcC9mcmFtZXdvcmsvZGF0YWFjY2Vzcy9tb2RlbC91c2Vycy5qcyIsInNvdXJjZXNDb250ZW50IjpbImNsYXNzIFVzZXJzQ2xhc3NNb2RlbCB7XHJcbn1cclxuZXhwb3J0ID0gVXNlcnNDbGFzc01vZGVsO1xyXG4iXX0=
