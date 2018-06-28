4ba61570924f6d6731b65f6de721d718dcef3308
Anil <anil.gadge@techprimelab.com>
2018-06-28 14:40:13 +0530

    [BuildInfo-432]staging dist deleted.

--- dist/server/prod/app/framework/dataaccess/model/UserModel.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAvZnJhbWV3b3JrL2RhdGFhY2Nlc3MvbW9kZWwvVXNlck1vZGVsLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
