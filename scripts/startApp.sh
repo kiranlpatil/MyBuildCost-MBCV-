a1791cdf9973a9fbd3e8213555dda1b6f63f71e5
Anil <anil.gadge@techprimelab.com>
2018-07-27 09:25:45 +0530

    [BI]Automation

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,5 +1,5 @@
 fuser kill `sudo lsof -t -i:8080`
-cd /home/bitnami/apps/costcontrol/costcontrol-automation/dist/
+cd /home/bitnami/apps/costcontrol-automation/dist/
 mkdir -p logs
 npm -v
 npm install
