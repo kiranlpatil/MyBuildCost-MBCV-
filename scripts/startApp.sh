6f58a38e249a507cb5ce56c2b628b75f7f5e2e33
Anil <anil.gadge@techprimelab.com>
2018-07-31 14:50:12 +0530

    [BI]Automation

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu/apps/costcontrol-automation/dist/
 mkdir -p logs
 chown -R ubuntu /home/ubuntu/apps/costcontrol-automation
 sudo npm -v
-npm install
-node app.server.prod.js --NODE_ENV=staging &> /dev/null 2> /dev/null < /dev/null &
+sudo npm install
+sudo node app.server.prod.js --NODE_ENV=staging &> /dev/null 2> /dev/null < /dev/null &
