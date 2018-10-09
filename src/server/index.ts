5282f085848f41386a9ddb02bb443d8a7af93af6
Lucky Vaishnav <lucky.vaishnav@techprimelab.com>
2018-10-09 17:03:43 +0530

    [BUILDINFO-168]  MBC Web application should get loaded in browser in less than 3-4 seconds

--- src/server/index.ts
+++ src/server/index.ts
@@ -62,11 +62,10 @@ export function init(port: number, mode: string, protocol: string, dist_runner: 
   //syncAtEveryFifteenMinute.start();
 
 
-  let sendProjectExpiryWarningMail = new CronJob('00 55 23 * * *', function() {
-      logger.debug('sendProjectExpiryWarningMail in debug mode');
+  let sendProjectExpiryWarningMail = new CronJob('00 50 23 * * *', function() {
+  //let sendProjectExpiryWarningMail = new CronJob('00 00 01 * * *', function() {
       let userService : UserService = new UserService();
-      let _loggerService: LoggerService = new LoggerService('sendProjectExpiryWarningMail');
-      _loggerService.logDebug('ProjectExpiryWarningMail started.');
+      let _loggerService: LoggerService = new LoggerService('uncaught exception Handler');
       userService.sendProjectExpiryWarningMails((error, success) => {
         if(error) {
           _loggerService.logError('Error in sendProjectExpiryWarningMail for users : ' +error);
@@ -231,8 +230,8 @@ export function init(port: number, mode: string, protocol: string, dist_runner: 
       /**
        * Client Dir
        */
-      _clientDir = './client/prod';
-      _serverDir = '/server/prod';
+      _clientDir = './dist/client/prod';
+      _serverDir = '/dist/server/prod';
 
       /**
        * Static.
@@ -249,7 +248,7 @@ export function init(port: number, mode: string, protocol: string, dist_runner: 
        * @param res {any}
        */
       var renderIndex = function (req: express.Request, res: express.Response) {
-        _clientDir = '/client/prod';
+        _clientDir = '/dist/client/prod';
         res.sendFile(path.resolve(__dirname + _clientDir + '/index.html'));
       };
 
