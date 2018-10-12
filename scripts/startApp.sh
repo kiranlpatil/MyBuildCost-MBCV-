d55063c8a46e4a446c5c45b05e35bda7f06b2c8b
Swapnil <swapnil.nakhate1010@gmail.com>
2018-10-12 14:36:27 +0530

    [BUILDINFO-1103] MBC Production automation - Final bash script permission error solved

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -2,8 +2,9 @@ sudo kill `sudo lsof -t -i:80`
 curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/MBC_ProdArtifacts.zip -u 'AnilGadge:Anil$1993'
 sudo apt-get update
 unzip MBC_ProdArtifacts.zip
-npm -v
-node -v
+sudo npm -v
+sudo node -v
 sudo npm install
 mkdir -p logs
+sudo rm -rf MBC_ProdArtifacts.zip
 node app.server.prod.js --NODE_ENV=production> /dev/null 2> /dev/null < /dev/null &
