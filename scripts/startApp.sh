e8af3ee584721fa946abfad44ce9023f855f6148
Swapnil <swapnil.nakhate1010@gmail.com>
2018-10-15 16:10:13 +0530

    [BUILDINFO-1103] MBC Production auto deployment new script

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -3,9 +3,10 @@ chown -R indrajeetmalave /home/indrajeetmalave/CostControl/costcontrol-prod-auto
 curl -O -L -o myBuildCost https://bitbucket.org/tplabs/costcontrol/downloads/MBC_ProdArtifacts.zip -u 'AnilGadge:Anil$1993'
 sudo apt-get update
 unzip MBC_ProdArtifacts.zip
+sudo rm -rf MBC_ProdArtifacts.zip
+cd dist
 sudo npm -v
 sudo node -v
-sudo npm install --unsafe -perm
 mkdir -p logs
-sudo rm -rf MBC_ProdArtifacts.zip
-sudo node app.server.prod.js --NODE_ENV=production> /dev/null 2> /dev/null < /dev/null &
+sudo npm install --unsafe -perm
+sudo node app.server.prod2.js --NODE_ENV=production> /dev/null 2> /dev/null < /dev/null &
