4fe3ef8fc8b85e568f0614c0a9d6b500a7e65487
Dhanashri123 <dhanashri.patil@techprimelab.com>
2018-10-19 17:33:21 +0530

    BI-1175 Total project slab or salable area should be displayed in common amenties-review comments

--- src/client/app/build-info/framework/project/report-templates/common-amenities-report/common-amenities-report.component.ts
+++ src/client/app/build-info/framework/project/report-templates/common-amenities-report/common-amenities-report.component.ts
@@ -18,6 +18,7 @@ export class CommonAmenitiesReportComponent {
   @Input() amenitiesReport: any;
   @Input() costingByUnit: any;
   @Input() costingByArea: any;
+  @Input() grandTotalOfArea: number;
 
   isBudgeted: boolean = false;
   isEstimated: boolean = false;
